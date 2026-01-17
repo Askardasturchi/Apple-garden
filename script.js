ac6c177743ce54bb7c79fca6db0a34370788a96e
Mamarajab <oddiyaskar2003@gmail.com>
2026-01-17 21:53:19 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Tugma bosilganda boshqa sahifaga yo'naltirish
-document.querySelectorAll('.animated-btn').forEach(btn => {
-    btn.addEventListener('click', function(e){
-        const href = btn.getAttribute('href');
-        if(href && href !== "#"){
-            window.location.href = href; // Menyu.html yoki boshqa sahifa
-        }
-    });
-});
