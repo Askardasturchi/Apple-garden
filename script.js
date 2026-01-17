fb5d66315916438d2794df8d78e0681799716b82
Mamarajab <oddiyaskar2003@gmail.com>
2026-01-17 21:05:32 +0500

    Change button click event to use 'href' attribute

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+// Tugma bosilganda boshqa sahifaga yo'naltirish
 document.querySelectorAll('.animated-btn').forEach(btn => {
-    btn.addEventListener('click', e => {
-        const target = btn.getAttribute('data-target');
-        if(target){
-            window.location.href = target;
+    btn.addEventListener('click', function(e){
+        const href = btn.getAttribute('href');
+        if(href && href !== "#"){
+            window.location.href = href; // Menyu.html yoki boshqa sahifa
         }
     });
 });
