786335194427221bc1a2e7713e49275b90ac989b
Mamarajab <oddiyaskar2003@gmail.com>
2026-01-17 20:22:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// Ideal hover va ripple animation tayyor
 document.querySelectorAll('.animated-btn').forEach(btn => {
     btn.addEventListener('click', e => {
-        // Agar href="#" bo'lsa keyinchalik JS orqali target yo'naltirish mumkin
-        const href = btn.getAttribute('href');
-        if(href && href !== "#"){
-            window.location.href = href; // Menyu.html yoki boshqa sahifa
+        const target = btn.getAttribute('data-target');
+        if(target){
+            window.location.href = target;
         }
     });
 });
